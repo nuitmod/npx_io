be44e63cae53507b3314a558e1248de197bcbd1d
nuitmod <wtvfqm@gmail.com>
2020-01-09 13:59:38 +0500

    deploy heroku

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
+var express = require("express");
+var app = require('express.io')();
+//app.http().io();
+var port=7076;
+//var path = require('path');
+//var app=express()
 
+app.get('/', function(req, res){
+  console.log("request was made from ", req.url);
+  //res.sendFile(__dirname + '/index.html');
+  res.send("io void deep web");
+})
+
+app.get('/io', function(req, res) {
+    req.io.route('some-cool-realtime-route');
+});
+
+
+console.log("io server vaiting on port", port);
+app.listen(port)
+
+
+
+
+/*
 var io = require('socket.io')(80);
 var cfg = require('./config.json');
 var tw = require('node-tweet-stream')(cfg);
@@ -7,3 +31,4 @@ tw.track('javascript');
 tw.on('tweet', function(tweet){
   io.emit('tweet', tweet);
 });
+*/
